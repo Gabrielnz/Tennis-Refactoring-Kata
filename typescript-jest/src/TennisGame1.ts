050c668aac0d0432e4b6cc2086027ee25a5dd617
gnunez <gnunez@aidacanarias.com>
2022-01-13 08:11:04 +0000

    re-adding constructor with players names

--- typescript-jest/src/TennisGame1.ts
+++ typescript-jest/src/TennisGame1.ts
@@ -1,12 +1,19 @@
 import { TennisGame } from './TennisGame';
 
 export class TennisGame1 implements TennisGame {
+  private player1Name: string;
+  private player2Name: string;
   private firstPlayerPoints: number = 0;
   private secondPlayerPoints: number = 0;
   private scores: string[] = ['Love', 'Fifteen', 'Thirty', 'Forty'];
 
+  constructor(player1Name: string, player2Name: string) {
+    this.player1Name = player1Name;
+    this.player2Name = player2Name;
+  }
+
   wonPoint(playerName: string): void {
-    if (playerName === 'player1')
+    if (playerName === this.player1Name)
       this.firstPlayerPoints += 1;
     else
       this.secondPlayerPoints += 1;
@@ -25,15 +32,15 @@ export class TennisGame1 implements TennisGame {
   private getScoreWhenAPlayerHasFourOrMorePoints() {
     const minusResult: number = this.firstPlayerPoints - this.secondPlayerPoints;
     if (minusResult === 1)
-      return 'Advantage player1';
+      return `Advantage ${this.player1Name}`;
 
     if (minusResult === -1)
-      return 'Advantage player2';
+      return `Advantage ${this.player2Name}`;
 
     if (minusResult >= 2)
-      return 'Win for player1';
+      return `Win for ${this.player1Name}`;
 
-    return 'Win for player2';
+    return `Win for ${this.player2Name}`;
   }
 
   private getScoreWhenPlayersHaveSamePoints() {
