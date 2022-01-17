f67be540bda8978076c7e8330ddab45e4d5a05ea
gnunez <gnunez@aidacanarias.com>
2022-01-17 08:09:41 +0000

    renaming parameters

--- typescript-jest/src/TennisGame3.ts
+++ typescript-jest/src/TennisGame3.ts
@@ -2,7 +2,7 @@ import { TennisGame } from './TennisGame';
 
 export class TennisGame3 implements TennisGame {
   private p2: number = 0;
-  private p1: number = 0;
+  private firstPlayerPoints: number = 0;
   private firstPlayerName: string;
   private secondPlayerName: string;
 
@@ -13,21 +13,21 @@ export class TennisGame3 implements TennisGame {
 
   getScore(): string {
     let s: string;
-    if (this.p1 < 4 && this.p2 < 4 && !(this.p1 + this.p2 === 6)) {
+    if (this.firstPlayerPoints < 4 && this.p2 < 4 && !(this.firstPlayerPoints + this.p2 === 6)) {
       const p: string[] = ['Love', 'Fifteen', 'Thirty', 'Forty'];
-      s = p[this.p1];
-      return (this.p1 === this.p2) ? s + '-All' : s + '-' + p[this.p2];
+      s = p[this.firstPlayerPoints];
+      return (this.firstPlayerPoints === this.p2) ? s + '-All' : s + '-' + p[this.p2];
     } else {
-      if (this.p1 === this.p2)
+      if (this.firstPlayerPoints === this.p2)
         return 'Deuce';
-      s = this.p1 > this.p2 ? this.firstPlayerName : this.secondPlayerName;
-      return (((this.p1 - this.p2) * (this.p1 - this.p2)) === 1) ? 'Advantage ' + s : 'Win for ' + s;
+      s = this.firstPlayerPoints > this.p2 ? this.firstPlayerName : this.secondPlayerName;
+      return (((this.firstPlayerPoints - this.p2) * (this.firstPlayerPoints - this.p2)) === 1) ? 'Advantage ' + s : 'Win for ' + s;
     }
   }
 
   wonPoint(playerName: string): void {
     if (playerName === 'player1')
-      this.p1 += 1;
+      this.firstPlayerPoints += 1;
     else
       this.p2 += 1;
   }
